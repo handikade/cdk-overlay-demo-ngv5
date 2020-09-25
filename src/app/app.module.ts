aba5bc8ba5cd6f228c449c6956df6483d9c3faaa
Dika Handika <dika.handika@paper.id>
2020-09-25 23:13:18 +0700

    initial setup (angular material, primeng, etc)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,46 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { OverlayModule } from '@angular/cdk/overlay';
+import { RouterModule, Routes } from '@angular/router';
+import { DataTableModule, SharedModule } from 'primeng/primeng';
 
 import { AppComponent } from './app.component';
+import { PageOneComponent } from './page-one/page-one.component';
+import { PageTwoComponent } from './page-two/page-two.component';
 
+const appRoutes: Routes = [
+  {
+    path: 'page-one',
+    component: PageOneComponent
+  },
+  {
+    path: 'page-two',
+    component: PageTwoComponent
+  },
+  {
+    path: '',
+    redirectTo: '/page-one',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageOneComponent,
+    PageTwoComponent
   ],
   imports: [
-    BrowserModule
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true }
+    ),
+    BrowserModule,
+    BrowserAnimationsModule,
+    OverlayModule,
+    DataTableModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
